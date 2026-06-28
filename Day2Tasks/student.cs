cfdbe097d87a9cdcf27bcc832de1e3ab7e54a157
ElakkiaKarunakaran <elakkia.karunakaran@psiog.com>
2026-06-28 19:28:51 +0530

    Day 2 - namespaces, identifiers, preprocessor

---
+++ Day2Tasks/student.cs
@@ -0,0 +1,24 @@
+﻿namespace SchoolManagement
+{
+	public class Student
+	{
+		#region Fields
+		private int age;
+		#endregion
+
+		#region Properties
+		public string Name { get; set; }
+		#endregion
+
+		#region Constructors
+		public Student() { }
+		#endregion
+
+		#region Methods
+		public void SayHello()
+		{
+			Console.WriteLine($"Hello, I'm {Name}");
+		}
+		#endregion
+	}
+}
