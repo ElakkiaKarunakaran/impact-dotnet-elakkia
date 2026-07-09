ed8d6dd58b495e548a468b0ab5e0cd9175b08d74
ElakkiaKarunakaran <elakkia.karunakaran@psiog.com>
2026-07-09 08:38:25 +0530

    Week 2 Day 4 - generics yield LINQ

---
+++ Week02/Day3Tasks/Generics, yield, LINQ/Generics, yield, LINQ/EvenNumbers.cs
@@ -0,0 +1,17 @@
+﻿public class EvenNumbers
+{
+	// IEnumerable<int> means "this method produces a sequence of ints"
+	// yield return means "hand over this one item, then PAUSE until next is needed"
+	public static IEnumerable<int> GetEvenNumbers(int max)
+	{
+		Console.WriteLine("-- Generator started --");
+
+		for (int i = 2; i <= max; i += 2)
+		{
+			Console.WriteLine($"  Yielding {i}");
+			yield return i; // pauses here, resumes when foreach asks for next item
+		}
+
+		Console.WriteLine("-- Generator finished --");
+	}
+}
